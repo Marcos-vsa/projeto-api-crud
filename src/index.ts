772a9a8116b04a700ce9a93bc8a7fa581fcf1562
Marcos Vinícius <123216797+Marcos-vsa@users.noreply.github.com>
2023-09-26 06:46:31 -0300

    Implementação do crud

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 import "reflect-metadata"
 import { AppDataSource} from "./database/data-source";
-console.log("hello word");
+import app from "./app";
 
 
 
 const main =async () => {
     try {
+        //conectando o banco de dados 
         await AppDataSource.initialize();
         console.log('Banco de dados conectado com sucesso!')
+
+        //inicializando express
+        app.listen(3000 , () =>{
+            console.log('Aplicação ouvindo na porta 3000')
+        })
     } catch (error) {
         console.log(error)
     }
