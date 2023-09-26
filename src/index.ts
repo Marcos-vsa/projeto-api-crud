c6cff9edc003c589c630bd5a3ba23ca423f73dc0
Marcos Vinícius <123216797+Marcos-vsa@users.noreply.github.com>
2023-09-26 02:23:00 -0300

    configuração do banco de dados

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello world");
+import "reflect-metadata"
+import { AppDataSource} from "./database/data-source";
+console.log("hello word");
+
+
+
+const main =async () => {
+    try {
+        await AppDataSource.initialize();
+        console.log('Banco de dados conectado com sucesso!')
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+main();
